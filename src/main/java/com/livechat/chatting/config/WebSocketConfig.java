7511cd4aa23b24df4f9cec3116cad99a0f167292
Aparna Misar <67634576+aparna906@users.noreply.github.com>
2021-10-16 11:56:39 +0530

    init

---
+++ src/main/java/com/livechat/chatting/config/WebSocketConfig.java
@@ -0,0 +1,43 @@
+package com.livechat.chatting.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/websocket").withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+    
+    @EnableWebSecurity
+    public class WebSecurity extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception { 
+            http
+            .cors().and()
+            .csrf().disable().authorizeRequests()
+            .antMatchers("/users").hasRole("manager")
+            .anyRequest().authenticated()
+            .and()
+            .formLogin();
+            
+           
+            
+        }
+}
+}
---
+++ src/test/java/com/livechat/chatting/ChattingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.livechat.chatting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChattingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
